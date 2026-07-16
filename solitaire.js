e8f89e7824b9ebb87a342da3c8d387bb94629212
Hau Nguyen <153048896+tmazsl1net@users.noreply.github.com>
2026-07-16 10:15:15 +0700

    fix: revert mouse offset subtraction to restore natural drag-and-drop hit boxes

--- solitaire.js
+++ solitaire.js
@@ -434,7 +434,7 @@
         if (clicked.pile === 'deck') {
             drawCardFromDeck();
         } else if (clicked.pile === 'waste' && clicked.card) {
-            // Kéo lá bài ngửa ra khay Waste (tính toán offset từ điểm chuột nhấp thật tới góc bài để tránh giật hình)
+            // Kéo lá bài ngửa ra khay Waste
             state.dragState = {
                 source: 'waste',
                 cards: [clicked.card],
@@ -442,7 +442,7 @@
                 startY: my,
                 currX: mx,
                 currY: my,
-                cardStartX: WASTE_X - (mx - WASTE_X)
+                cardStartX: WASTE_X
             };
         } else if (clicked.pile === 'tableau' && clicked.cardIdx !== -1) {
             const card = clicked.card;
@@ -460,7 +460,7 @@
                     startY: my,
                     currX: mx,
                     currY: my,
-                    cardStartX: colX - (mx - colX)
+                    cardStartX: colX
                 };
             }
         }
